f93af4c7e4d733e99a72ef75c03e51c97df3863a
Matthew Wastrodowski <towski@gmail.com>
2013-12-20 13:31:52 -0800

    fix ambiguous

--- archive.rb
+++ archive.rb
@@ -1,6 +1,6 @@
 require_relative 'application'
 
-insert_sql = "INSERT INTO old_trades (id, date, price, amount, tid, price_currency, item, trade_type) SELECT * FROM trades where date < now() - interval 360 minute ON DUPLICATE KEY UPDATE tid=tid;"
+insert_sql = "INSERT INTO old_trades (id, date, price, amount, tid, price_currency, item, trade_type) SELECT * FROM trades where date < now() - interval 360 minute ON DUPLICATE KEY UPDATE old_trades.tid=old_trades.tid;"
 delete_sql = "DELETE FROM trades where date < now() - interval 360 minute"
 
 connection = ActiveRecord::Base.connection
