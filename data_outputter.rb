bf1c9b6c4d17ef2f4a3b0004b32f0fcef7219dee
Matthew Wastrodowski <towski@gmail.com>
2013-12-20 22:18:27 -0800

    separate the files

--- data_outputter.rb
+++ data_outputter.rb
@@ -1,10 +1,16 @@
 require_relative 'application'
 
-filename = ARGV[0]
+dir = ARGV[0]
 
-file = File.open(filename, "w")
-file << "date\tprice\n"
-MyTrade.find_each(:conditions => "date > now() - interval 2 day") do |trade|
-  file << "#{trade.date.strftime("%Y-%m-%d-%H:%M:%S")}\t#{trade.price}\n"
+buy_file = File.open("%s/buys.tsv" % dir, "w")
+sell_file = File.open("%s/sells.tsv" % dir, "w")
+buy_file << "date\tprice\n"
+sell_file << "date\tprice\n"
+MyTrade.find_each(:conditions => "date > now() - interval 2 day and trade_type = 'buy'") do |trade|
+  buy_file << "#{trade.date.strftime("%Y-%m-%d-%H:%M:%S")}\t#{trade.price}\n"
 end
-file.close
+MyTrade.find_each(:conditions => "date > now() - interval 2 day and trade_type = 'sell'") do |trade|
+  sell_file << "#{trade.date.strftime("%Y-%m-%d-%H:%M:%S")}\t#{trade.price}\n"
+end
+buy_file.close
+sell_file.close
