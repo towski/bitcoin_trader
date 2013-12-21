c44ec6cc86a5849a295cc0da0ca40b6aa361edf1
Matthew Wastrodowski <towski@gmail.com>
2013-12-20 20:32:32 -0800

    tabs

--- data_outputter.rb
+++ data_outputter.rb
@@ -3,8 +3,8 @@ require_relative 'application'
 filename = ARGV[0]
 
 file = File.open(filename, "w")
-file << "date price\n"
+file << "date\tprice\n"
 MyTrade.find_each(:conditions => "date > now() - interval 2 day") do |trade|
-  file << "#{trade.date.strftime("%Y-%m-%d-%H:%M:%S")} #{trade.price}\n"
+  file << "#{trade.date.strftime("%Y-%m-%d-%H:%M:%S")}\t#{trade.price}\n"
 end
 file.close
