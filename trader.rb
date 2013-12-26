a9f5d25acc3c9ddbd3380db1c6802ed2d5f8d9a3
Matthew Wastrodowski <towski@gmail.com>
2013-12-26 15:00:54 -0800

    use the 3 minute average instead of 1

--- trader.rb
+++ trader.rb
@@ -56,8 +56,8 @@ class Trader
     loop do
       begin
         ma_x_minutes_ago = "select sum(average) / count(*) from (select avg(price) as average, date, ROUND(UNIX_TIMESTAMP(date)/(%s * 60)) AS timekey from trades where item = '#{$currency}' and date between DATE_SUB(UTC_TIMESTAMP() - INTERVAL %s minute, INTERVAL %s minute) and UTC_TIMESTAMP() - INTERVAL %s minute group by timekey) as average_table;"
-        ma3 = ActiveRecord::Base.connection.execute(ma_x_minutes_ago % [1, 0, (1 * 24), 0]).to_a.first.first
-        short_ma3 = ActiveRecord::Base.connection.execute(ma_x_minutes_ago % [1, 0, (1 * 7), 0]).to_a.first.first
+        ma3 = ActiveRecord::Base.connection.execute(ma_x_minutes_ago % [3, 0, (3 * 24), 0]).to_a.first.first
+        short_ma3 = ActiveRecord::Base.connection.execute(ma_x_minutes_ago % [3, 0, (3 * 7), 0]).to_a.first.first
         if @index % 50 == 0
           puts "status: %s %s %s %s" % [@last_price, @amount.round(4), @money, Time.now]
           puts "long term: %s" % ma3.round(4)
